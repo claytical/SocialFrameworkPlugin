a8ea795d0148537e16ecc6fb6188a6a27c91b3a7
Patrick Clay <patrickclay86@gmail.com>
2012-10-13 21:35:16 -0500

    Added ability to share images

--- SocialFrameworkPlugin/Plugins/SocialFrameworkPlugin.m
+++ SocialFrameworkPlugin/Plugins/SocialFrameworkPlugin.m
@@ -23,9 +23,15 @@
     
     
     NSString *textToShare = [arguments objectAtIndex:0];
+
+    NSData *imageData = [NSData dataFromBase64String:[arguments objectAtIndex:1]];
+    UIImage *imageToShare = [UIImage imageWithData:imageData];
+
     CDVPluginResult *result;
+
+
     
-    NSArray *activityItems = @[textToShare];
+    NSArray *activityItems = @[textToShare, imageToShare];
     UIActivityViewController *activityController = [[UIActivityViewController alloc]
                                                     initWithActivityItems:activityItems applicationActivities:nil];
     
@@ -58,9 +64,10 @@
     
     NSString *textToShare = [arguments objectAtIndex:0];
     
+    NSData *imageToShare = [NSData dataFromBase64String:[arguments objectAtIndex:1]];
+
     //TODO: figure out how arrays work within cordova to allow image and url sharing
     /*
-     NSString *imageToShare = [arguments objectAtIndex:1];
      NSString *urlToShare = [arguments objectAtIndex:2];
      */
     SLComposeViewController *fbSLComposerSheet;
@@ -71,7 +78,7 @@
         fbSLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
         
         [fbSLComposerSheet setInitialText:[NSString stringWithFormat:textToShare,fbSLComposerSheet.serviceType]];
-        //        [fbSLComposerSheet addImage:[UIImage imageNamed:imageToShare]];
+        [fbSLComposerSheet addImage:[UIImage imageWithData:imageToShare]];
         //        [fbSLComposerSheet addURL:[NSURL URLWithString:urlToShare]];
         [self.viewController presentViewController:fbSLComposerSheet animated:YES completion:nil];
     }
